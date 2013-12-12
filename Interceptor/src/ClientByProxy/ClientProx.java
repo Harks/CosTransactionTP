6dae48487e956da1230333ea3dcb40b96427e9f7
Yannis <yflechel@gmail.com>
2013-12-12 18:43:04 +0100

    Trois Handle Creation classe transactor avec méthode vide begin, commit,rollback

--- Interceptor/src/ClientByProxy/ClientProx.java
+++ Interceptor/src/ClientByProxy/ClientProx.java
@@ -6,13 +6,16 @@ import CosTransaction.*;
 public class ClientProx {
 
 	public static void main(String[] args) throws RemoteException {
-		IAvailableseat aseat = (IAvailableseat) Proxy.newProxyInstance(IAvailableseat.class.getClassLoader(),new Class[] {IAvailableseat.class}, new InterceptorHandler());
+		Transactor transac = new Transactor();
+		transac.begin();
+		IAvailableseat aseat = (IAvailableseat) Proxy.newProxyInstance(IAvailableseat.class.getClassLoader(),new Class[] {IAvailableseat.class}, transac.getAvailhandler());
 		String msg ="Send by ClientProx";
 		aseat.showinfo(msg);
-		ILockerSeat locks = (ILockerSeat) Proxy.newProxyInstance(ILockerSeat.class.getClassLoader(), new Class[] {ILockerSeat.class},new InterceptorHandler());
+		ILockerSeat locks = (ILockerSeat) Proxy.newProxyInstance(ILockerSeat.class.getClassLoader(), new Class[] {ILockerSeat.class},transac.getLockhandler());
 		locks.getSideBySideseats(null, 0);
-		ISideBySide sides =  (ISideBySide) Proxy.newProxyInstance(ISideBySide.class.getClassLoader(), new Class[] {ISideBySide.class},new InterceptorHandler());
+		ISideBySide sides =  (ISideBySide) Proxy.newProxyInstance(ISideBySide.class.getClassLoader(), new Class[] {ISideBySide.class},transac.getSidehandler());
 		sides.lockseats(null);
+		transac.commit();
 	}
 
 }
---
+++ Interceptor/src/CosTransaction/Transactor.java
@@ -0,0 +1,22 @@
+package CosTransaction;
+
+public class Transactor {
+	private LockerSeatHandler lockhandler;
+	private SideBySideHandler sidehandler;
+	private AvailableHandler availhandler;
+	public void begin(){}
+	public void commit(){}
+	public void rollback(){}
+	public LockerSeatHandler getLockhandler() {
+		lockhandler = new LockerSeatHandler();
+		return lockhandler;
+	}
+	public SideBySideHandler getSidehandler() {
+		sidehandler = new SideBySideHandler();
+		return sidehandler;
+	}
+	public AvailableHandler getAvailhandler() {
+		availhandler = new AvailableHandler();
+		return availhandler;
+	}
+}
