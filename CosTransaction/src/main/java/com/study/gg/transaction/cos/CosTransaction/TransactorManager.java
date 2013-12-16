3fd0224c6ef5c3b395e035762beb9238a158ad86
Loic <lo.regnault@gmail.com>
2013-12-16 20:45:30 +0100

    Push singleton

--- CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
+++ CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
@@ -1,28 +1,39 @@
 package com.study.gg.transaction.cos.CosTransaction;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TransactorManager {
 
-	private Map<Integer,ArrayList<Transactor>> transactions = new HashMap<Integer,ArrayList<Transactor>>();
-	
-	private TransactorManager(){
-		//TODO Singleton to only have one manager
-	}
-	
-	public void addTransaction(Transactor transaction, int id){
-		transactions.get(id).add(transaction);
-	}
-	
-	static TransactorManager getInstance(){
-		//TODO singleton
-		return null;
+	private Map<Integer, Transactor> transactions;
+	private static TransactorManager instance;
+
+	private TransactorManager() {
+		this.transactions = new HashMap<Integer, Transactor>();
 	}
 
-	public Map<Integer, ArrayList<Transactor>> getTransactions() {
+	public void addTransaction(Transactor transaction, int id) {
+		synchronized (this) {
+			if (transactions.containsKey(id)) {
+				instance.addTransaction(transaction, id + 1);
+			} else {
+				transactions.put(id, transaction);
+			}
+		}
+	}
+
+	static TransactorManager getInstance() {
+		if (instance == null) {
+			instance = new TransactorManager();
+		}
+		return instance;
+	}
+
+	public Map<Integer, Transactor> getTransactions() {
 		return transactions;
 	}
 
+	public boolean rollBackTransaction(){
+		return true;
+	}
 }
