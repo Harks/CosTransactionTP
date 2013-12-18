eb43339cddaf005420337cbee0df7e54d9d14c18
Loic <lo.regnault@gmail.com>
2013-12-18 12:01:49 +0100

    Comment in transaction manager

--- CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
+++ CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
@@ -2,7 +2,13 @@ package com.study.gg.transaction.cos.CosTransaction;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * This class store every transaction wich is made. And should also store
+ * the data concerne by the current transaction, the data stored is the value
+ * before the transaction in order to be able to rollback the transaction.
+ * @author loicregnault
+ *
+ */
 public class TransactorManager {
 
 	private Map<Integer, Transactor> transactions;
