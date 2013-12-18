c054f9c5ba1dc1773d4d8619616fc0f04b2f180d
Loic <lo.regnault@gmail.com>
2013-12-18 23:08:25 +0100

    Commit

--- Client/src/main/java/com/study/gg/transaction/Client/ClientProx.java
+++ Client/src/main/java/com/study/gg/transaction/Client/ClientProx.java
@@ -13,21 +13,32 @@ public class ClientProx {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException, UnknownHostException, NotBoundException {
 		ArrayList<Seat> myseats;
+		ArrayList<Seat> myseats2;
 		Transactor transac = new Transactor(1);
 		Transactor transac2 = new Transactor(2);
 		transac.begin();
 		IAvailableseat aseat = (IAvailableseat)transac.addRessource(IAvailableseat.class);
 		ISideBySide sides = (ISideBySide)transac.addRessource(ISideBySide.class);
 		ILockerSeat locks = (ILockerSeat)transac.addRessource(ILockerSeat.class);
+
+		IAvailableseat aseat2 = (IAvailableseat)transac2.addRessource(IAvailableseat.class);
+		ISideBySide sides2 = (ISideBySide)transac2.addRessource(ISideBySide.class);
+		ILockerSeat locks2 = (ILockerSeat)transac2.addRessource(ILockerSeat.class);
 		
 		//System.out.println(aseat.getserviceAdresse());
 		myseats = aseat.getAvailableSeat();
 		System.out.println("After AvailableSeat "+myseats);
-		myseats = sides.getSideBySideseats(myseats, 3);
+		myseats2 = aseat2.getAvailableSeat();
+		System.out.println("After AvailableSeat 2 "+myseats);
+		myseats2 = sides2.getSideBySideseats(myseats, 2);
+		System.out.println("After SideBySideseats "+myseats);
+		myseats = sides.getSideBySideseats(myseats, 2);
 		System.out.println("After SideBySideseats "+myseats);
 		myseats = locks.lockseats(myseats);
-		transac.commit();
 		System.out.println("Final "+myseats);
+		myseats2 = locks2.lockseats(myseats);
+		transac.commit();
+		System.out.println("Final "+myseats2);
 	}
 
 }
--- CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
+++ CosTransaction/src/main/java/com/study/gg/transaction/cos/CosTransaction/TransactorManager.java
@@ -1,13 +1,25 @@
 package com.study.gg.transaction.cos.CosTransaction;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.study.gg.transaction.Servicescommon.IAvailableseat;
+import com.study.gg.transaction.Servicescommon.ILockerSeat;
+import com.study.gg.transaction.Servicescommon.ISideBySide;
+import com.study.gg.transaction.Servicescommon.ServiceInterface;
 
 public class TransactorManager {
 
 	private Map<Integer,ArrayList<Transactor>> transactions;
 	static private TransactorManager manager;
+	static LockerSeatHandler lockhandler;
+	static SideBySideHandler sidehandler;
+	static AvailableHandler availhandler;
+	static IAvailableseat available;
+	static ILockerSeat lock;
+	static ISideBySide side;
 	
 	private TransactorManager(){
 		this.transactions = new HashMap<Integer,ArrayList<Transactor>>();
@@ -26,6 +38,45 @@ public class TransactorManager {
 
 	public Map<Integer, ArrayList<Transactor>> getTransactions() {
 		return transactions;
+	}
+	
+	static ServiceInterface getHandler(Class service){
+		System.out.println(service.getSimpleName());
+		if (service.getSimpleName().toString().equalsIgnoreCase("IAvailableseat")){
+			if(availhandler == null){
+				available = (IAvailableseat) Proxy.newProxyInstance(IAvailableseat.class.getClassLoader(),new Class[] {IAvailableseat.class},TransactorManager.getAvailhandler());
+			}
+			return available;
+		}
+		else if (service.getSimpleName().toString().equalsIgnoreCase("ILockerSeat")){
+			if(lock == null ){
+				lock = (ILockerSeat) Proxy.newProxyInstance(ILockerSeat.class.getClassLoader(),new Class[] {ILockerSeat.class},TransactorManager.getLockhandler());
+			}
+			return lock;
+		}
+		else if(service.getSimpleName().toString().equalsIgnoreCase("ISideBySide")) {
+			if(side == null){
+				side = (ISideBySide) Proxy.newProxyInstance(ISideBySide.class.getClassLoader(),new Class[] {ISideBySide.class},TransactorManager.getSidehandler());
+			}
+			return side;
+		}else {
+			return null;
+		}
+	}
+	
+	static LockerSeatHandler getLockhandler() {
+		lockhandler = new LockerSeatHandler();
+		return lockhandler;
+	}
+
+	static SideBySideHandler getSidehandler() {
+		sidehandler = new SideBySideHandler();
+		return sidehandler;
+	}
+
+	static AvailableHandler getAvailhandler() {
+		availhandler = new AvailableHandler();
+		return availhandler;
 	}
 
 }
